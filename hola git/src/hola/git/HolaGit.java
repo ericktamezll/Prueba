1ca748331e23144299f6b8d9481082b77527d6da
Kristel <Kristel@Kristel-PC>
2016-05-04 18:14:01 -0500

    la kris locota edito

--- hola git/src/hola/git/HolaGit.java
+++ hola git/src/hola/git/HolaGit.java
@@ -18,8 +18,18 @@ public class HolaGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        JOptionPane.showMessageDialog(null, "hola k hace");
+        JOptionPane.showMessageDialog(null, "oki");
         //hola k ase 
+        //este es un proyecto que hize en mi compu :V
+        //  modificale algo :V lo que quie      
+        //otra cosa crea una clase un jframe o algo
+        int a, b ,c ;
+         a = 3;
+         b = 4;
+          c= a + b;
+          System.out.println(c);
+                 
+        //bien ahora fijate lo que voy a hacer ok?
     }
     
 }
